31af512c6f0c3b61395fa8de110b2bc0b7866298
Radomir Epur <52368972+radomir9720@users.noreply.github.com>
2023-06-24 17:39:14 +0300

    chore: improve text style parsing logic (#1)
    * chore: v0.0.2
    
    ---------
    
    Co-authored-by: Radomir Epur <radomir9720@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.2
+
+* **CHORE**: improve text style parsing logic
+
 # 0.0.1
 
 * **CHORE**: initial release
--- src/converters/text_style_converters.ts
+++ src/converters/text_style_converters.ts
@@ -1,48 +1,163 @@
 import { PropertyConverter } from "../models/property_converter";
 import * as vscode from "vscode";
+import { rgbaToHex } from "./color_converters";
+
+// Color
+const hex3Regexp = /color: #([0-9a-fA-F]{3});/;
+const hex6Regexp = /color: #([0-9a-fA-F]{6});/;
+const hex8Regexp = /color: #([0-9a-fA-F]{6})([0-9a-fA-F]{2});/;
+const rgbaRegexp = /rgba\((\d+),\s?(\d+),\s?(\d+),\s?(\d?.?\d)\);/;
+//
+const fontStyleRegexp = /font-style: (\w+);/;
+const textAlignRegexp = /text-align: .+;/;
+const fontSizeRegexp = /font-size: (\d+)px;/;
+const fontFamilyRegexp = /font-family: (.*);/;
+const fontWeightRegexp = /font-weight: (\d+);/;
+const lineHeightPxRegexp = /line-height: (\d+)px;/;
+const lineHeightPercentRegexp = /line-height: (\d+)%;/;
+const letterSpacingPxRegexp = /letter-spacing: (\-?\d?\.?\d+)px;/;
+const letterSpacingEmRegexp = /letter-spacing: (\-?\d?\.?\d+)em;/;
+const textDecorationRegexp = /text-decoration-line: (\w+);/;
+
+// Keys
+const kColorKey = "color";
+const kFontStyleKey = "fontStyle";
+const kFontFamilyKey = "fontFamily";
+const kFontWeightKey = "fontWeight";
+const kLetterSpacingKey = "letterSpacing";
+const kTextDecorationKey = "decoration";
+const kFontSizeKey = "fontSize";
+const kLineHeightKey = "height";
 
 export const textStyleConverter: PropertyConverter = {
-  regexp:
-    /font-family: (.+);\nfont-size: (\d+)px;\n(font-style: ([\w]+);\n)?font-weight: (\d+);\nline-height: (\d+)px;\nletter-spacing: (0?.?\d+)em;\n(text-align: (.+));/g,
-  replacer: (_, ff, size, __, style, fw, lh, ls, ta) => {
+  regexp: /.*/gs,
+  replacer: (all) => {
     const textStyleProps = vscode.workspace.getConfiguration(
       "f2f-properties-conveter.textStyle"
     );
-    const wrapInClass = textStyleProps.get("wrapTextStyleInClass");
+    const wrapInClass = textStyleProps.get("wrapTextStyleInClass") !== false;
+
+    let value = all;
+    const propertiesMap = new Map<string, any>();
+    popMatch(hex3Regexp, (_, color) =>
+      propertiesMap.set(
+        kColorKey,
+        formatColor(`${color}${color}`, !wrapInClass)
+      )
+    );
+    popMatch(hex6Regexp, (_, color) => {
+      propertiesMap.set(kColorKey, formatColor(color, !wrapInClass));
+    });
+    popMatch(hex8Regexp, (_, color, opacity) =>
+      propertiesMap.set(kColorKey, formatColor(color, !wrapInClass, opacity))
+    );
+    popMatch(rgbaRegexp, (_, r, g, b, a) => {
+      propertiesMap.set(
+        kColorKey,
+        formatColor(
+          `${rgbaToHex(r)}${rgbaToHex(g)}${rgbaToHex(b)}`,
+          !wrapInClass,
+          rgbaToHex(a, true)
+        )
+      );
+    });
+    popMatch(fontStyleRegexp, (_, style) =>
+      propertiesMap.set(kFontStyleKey, formatFontStyle(style))
+    );
+    popMatch(textAlignRegexp, () => {});
+    popMatch(fontSizeRegexp, (_, size) =>
+      propertiesMap.set(kFontSizeKey, size)
+    );
+    popMatch(fontFamilyRegexp, (_, ff) =>
+      propertiesMap.set(kFontFamilyKey, formatFontFamily(ff))
+    );
+    popMatch(fontWeightRegexp, (_, fw) =>
+      propertiesMap.set(kFontWeightKey, formatFontWeight(fw))
+    );
+    popMatch(lineHeightPxRegexp, (_, lh) => {
+      doIfSizeIsDefined((size) => {
+        const height = parseInt(lh) / size;
+        propertiesMap.set(kLineHeightKey, height.toFixed(2));
+      });
+    });
+    popMatch(lineHeightPercentRegexp, (_, lh) => {
+      doIfSizeIsDefined((size) => {
+        const height = (parseInt(lh) * size) / 100;
+        propertiesMap.set(kLineHeightKey, height.toFixed(2));
+      });
+    });
+    popMatch(letterSpacingPxRegexp, (_, ls) =>
+      propertiesMap.set(kLetterSpacingKey, ls)
+    );
+    popMatch(letterSpacingEmRegexp, (_, ls) => {
+      doIfSizeIsDefined((size) => {
+        const pasteZeroLetterSpacingWhenItsUndefined = textStyleProps.get(
+          "pasteZeroLetterSpacingWhenItsUndefined"
+        );
+        if (!ls || ls === "0") {
+          if (pasteZeroLetterSpacingWhenItsUndefined) {
+            propertiesMap.set(kLetterSpacingKey, "0");
+          }
+          return;
+        }
+        propertiesMap.set(kLetterSpacingKey, `${size * parseFloat(ls)}`);
+      });
+    });
+    popMatch(textDecorationRegexp, (_, td) => {
+      if (td === "strikethrough") {
+        return propertiesMap.set(
+          kTextDecorationKey,
+          formatTextDecoration("lineThrough")
+        );
+      }
+      propertiesMap.set(kTextDecorationKey, formatTextDecoration(td));
+    });
+
     const pasteFontStyleNormalWhenItsUndefined = textStyleProps.get(
       "pasteFontStyleNormalWhenItsUndefined"
     );
-    const pasteZeroLetterSpacingWhenItsUndefined = textStyleProps.get(
-      "pasteZeroLetterSpacingWhenItsUndefined"
-    );
+    if (pasteFontStyleNormalWhenItsUndefined === true) {
+      if (!propertiesMap.has(kFontStyleKey)) {
+        propertiesMap.set(kFontStyleKey, formatFontStyle("normal"));
+      }
+    }
 
-    const height = parseInt(lh) / parseInt(size);
-    const spacing =
-      ls === "0"
-        ? pasteZeroLetterSpacingWhenItsUndefined
-          ? "spacing: 0, "
-          : ""
-        : `${parseInt(size) * parseFloat(ls)}, `;
+    if (value.trim() !== "" || propertiesMap.size < 3) {
+      return all;
+    }
 
-    const fontStyle = style
-      ? `fontStyle: FontStyle.${style}, `
-      : pasteFontStyleNormalWhenItsUndefined
-      ? "fontStyle: FontStyle.normal, "
-      : "";
-    const fontFamily = formatFontFamily(ff);
-    const properties =
-      `height: ${height}, ` +
-      `fontSize: ${size}, ` +
-      `fontWeight: FontWeight.w${fw}, ` +
-      `${fontStyle}` +
-      `${spacing}` +
-      `${fontFamily}`;
+    const properties = Array.from(propertiesMap)
+      .map((e) => `${e[0]}: ${e[1]}`)
+      .join(",\n");
+
     if (wrapInClass) {
-      return `const TextStyle(${properties})`;
+      return `const TextStyle(\n${properties},\n)`;
     }
     return properties;
+
+    function popMatch(
+      regexp: RegExp,
+      replacer: (substring: string, ...args: any[]) => void
+    ): void {
+      value = value.replace(regexp, (all, ...segments) => {
+        replacer(all, ...segments);
+        return "";
+      });
+    }
+
+    function doIfSizeIsDefined(whenDefined: (size: number) => void): void {
+      const size = propertiesMap.get(kFontSizeKey);
+      if (size) {
+        whenDefined(parseInt(size));
+      }
+    }
   },
 };
+
+function formatColor(rgb: string, makeConst: boolean, opacity = "FF"): string {
+  const value = "0x" + `${opacity}${rgb}`.toUpperCase();
+  return `${makeConst ? "const " : ""}Color(${value})`;
+}
 
 export const fontLineHeightConverter: PropertyConverter = {
   regexp:
@@ -79,12 +194,22 @@ export const fontLetterSpacingConverter: PropertyConverter = {
 };
 
 export const fontFamilyConverter: PropertyConverter = {
-  regexp: /font-family: (.+);?/g,
+  regexp: RegExp(fontFamilyRegexp, "g"),
   replacer: (_, ff) => {
     const fontFamily = formatFontFamily(ff);
     return `fontFamily: ${fontFamily},`;
   },
 };
+
+function formatFontStyle(fs: string) {
+  return `FontStyle.${fs}`;
+}
+function formatFontWeight(fw: string) {
+  return `FontWeight.w${fw}`;
+}
+function formatTextDecoration(td: string) {
+  return `TextDecoration.${td}`;
+}
 
 function formatFontFamily(ff: string) {
   const fontFamilyQuotesId = vscode.workspace
@@ -99,26 +224,26 @@ function formatFontFamily(ff: string) {
     case "single":
       fontFamilyQuotes = "'";
   }
-  const fontFamily = `fontFamily: ${fontFamilyQuotes}${ff}${fontFamilyQuotes}`;
+  const fontFamily = `${fontFamilyQuotes}${ff}${fontFamilyQuotes}`;
   return fontFamily;
 }
 
 export const fontStyleConverter: PropertyConverter = {
-  regexp: /font-style: (\w+);?/g,
+  regexp: RegExp(fontStyleRegexp, "g"),
   replacer: (_, fs) => {
-    return `fontStyle: FontStyle.${fs},`;
+    return `${kFontStyleKey}: ${formatFontStyle(fs)},`;
   },
 };
 
 export const fontSizeConverter: PropertyConverter = {
-  regexp: /font-size: (\d+)px;?/g,
+  regexp: RegExp(fontSizeRegexp, "g"),
   replacer: (_, fs) => {
-    return `fontSize: ${fs},`;
+    return `${kFontSizeKey}: ${fs},`;
   },
 };
 export const fontWeightConverter: PropertyConverter = {
-  regexp: /font-weight: (\d+);?/g,
+  regexp: RegExp(fontWeightRegexp, "g"),
   replacer: (_, fw) => {
-    return `fontWeight: FontWeight.w${fw},`;
+    return `${kFontWeightKey}: ${formatFontWeight(fw)},`;
   },
 };
